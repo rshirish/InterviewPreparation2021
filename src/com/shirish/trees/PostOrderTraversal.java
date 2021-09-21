1a7a3e396857b71713c5d66e225009c55c88aebc
rshirish <rshirish26@gmail.com>
2021-09-21 18:48:26 +0530

    Tress questions
    Tress questions

---
+++ src/com/shirish/trees/PostOrderTraversal.java
@@ -0,0 +1,30 @@
+package com.shirish.trees;
+
+public class PostOrderTraversal {
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(6);
+
+        root.right.left = new Node(7);
+        root.right.right = new Node(8);
+        root.right.right.left = new Node(9);
+        root.right.right.right = new Node(10);
+        //Left  Right  Root
+        postOrderTraversal(root);
+
+    }
+
+    private static void postOrderTraversal(Node node) {
+        if(node == null)
+            return;
+
+        postOrderTraversal(node.left);
+        postOrderTraversal(node.right);
+        System.out.print(node.data + " ,");
+    }
+}
