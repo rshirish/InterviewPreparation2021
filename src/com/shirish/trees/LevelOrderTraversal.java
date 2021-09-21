123f039a0c3818acc368fa1677ace4084f012d6f
rshirish <rshirish26@gmail.com>
2021-09-21 19:47:37 +0530

    Create LevelOrderTraversal.java

---
+++ src/com/shirish/trees/LevelOrderTraversal.java
@@ -0,0 +1,48 @@
+package com.shirish.trees;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(6);
+
+        root.right.left = new Node(7);
+        root.right.right = new Node(8);
+        root.right.right.left = new Node(9);
+        root.right.right.right = new Node(10);
+        //Left  Right  Root
+        levelOrderTraversal(root);
+
+    }
+
+    private static void levelOrderTraversal(Node root) {
+
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        Queue<Node> queue = new LinkedList<Node>();
+        if(root == null) return;
+        queue.offer(root);
+
+        while(!queue.isEmpty())
+        {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i< levelNum; i++)
+            {
+                if(queue.peek().left!= null)
+                    queue.add(queue.peek().left);
+                if(queue.peek().right!= null)
+                    queue.add(queue.peek().right);
+                subList.add(queue.poll().data);
+            }
+            result.add(subList);
+        }
+        System.out.println(result);
+    }
+}
